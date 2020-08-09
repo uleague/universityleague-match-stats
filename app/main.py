76c36758f1689005aa800097f3176bf1e20494e2
semenodp <dmitry.p.semenov@vanderbilt.edu>
2020-08-09 16:59:44 -0500

    convert port to int

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import gevent
 from api import app, worker
 from settings import Config
 
-http_server = WSGIServer(("0.0.0.0", Config.PORT), app)
+http_server = WSGIServer(("0.0.0.0", int(Config.PORT)), app)
 
 server = gevent.spawn(http_server.serve_forever)
 bot = gevent.spawn(worker.prompt_login)
