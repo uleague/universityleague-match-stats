4502180b485f335c8f66a8d6be64b6497d8677cb
semenodp <dmitry.p.semenov@vanderbilt.edu>
2020-12-03 11:57:51 -0600

    add sentry

--- app/server.py
+++ app/server.py
@@ -4,11 +4,15 @@ gevent.monkey.patch_all()
 
 from gevent.pywsgi import WSGIServer
 import gevent
+import os
 
 from . import steam_bot
 from .api import create_app
 from .settings import Config
 
+import sentry_sdk
+
+sentry_sdk.init(os.getenv("SENTRY_DSN"), traces_sample_rate=0)
 
 def init():
     app = create_app()
